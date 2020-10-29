a2ee430fa9b8ed0d6558930dd658656e3ffb9761
Wu Yaohua <wuyaohua@spicespirit.com>
2020-10-29 10:21:00 +0800

    高亮匹配括号; 自动补齐括号等

--- lisp/init-misc.el
+++ lisp/init-misc.el
@@ -5,10 +5,26 @@
   :init (setq-default tab-width 4
 		              indent-tabs-mode nil))
 
-;;; 显示行号
 (use-package emacs
   :init
+  ;;; 显示行号
   (setq display-line-numbers-type 'relative)  ; relative=相对, visual=正常
-  (global-display-line-numbers-mode t))
+  (global-display-line-numbers-mode t)
+  ;;; 内置的高亮匹配括号
+  (show-paren-mode 1)
+  ;;; 自动补齐括号等
+  (electric-pair-mode 1)
+  (setq electric-pair-pairs '(
+                              (?\" . ?\")
+                              (?\' . ?\')
+                              (?\{ . ?\})
+                              (?\( . ?\))
+                              (?\[ . ?\])))
 
+;;; 高亮匹配括号
+(use-package highlight-parentheses
+  :init
+  (highlight-parentheses-mode t)
+  (global-highlight-parentheses-mode t))
+  
 (provide 'init-misc)
