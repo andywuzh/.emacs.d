4d9706ba32a7e35fb4b0bc81e8cf13465807b4d6
Wu Yaohua <wuyaohua@spicespirit.com>
2020-11-27 15:19:38 +0800

    fonts setting

--- lisp/init-ui.el
+++ lisp/init-ui.el
@@ -14,7 +14,21 @@
   (sml/setup))
 
 ;;; 字体
-(use-package cnfonts
-  :init (cnfonts-enable))
+;(use-package cnfonts
+;  :init (cnfonts-enable))
+(set-face-attribute
+ 'default nil
+ ;:font (font-spec :name "-PfEd-DejaVu Sans Mono-normal-normal-normal-*-*-*-*-*-m-0-iso10646-1"
+ :font (font-spec :name "-CTDB-FuraMono Nerd Font Mono-normal-normal-normal-*-16-*-*-*-*-0-iso10646-1"                  :weight 'normal
+                  :slant 'normal
+                  :size 9.0))
+(dolist (charset '(kana han symbol cjk-misc bopomofo))
+  (set-fontset-font
+   (frame-parameter nil 'font)
+   charset
+   (font-spec :name "-WQYF-文泉驿等宽微米黑-normal-normal-normal-*-*-*-*-*-*-0-iso10646-1"
+              :weight 'normal
+              :slant 'normal
+              :size 10.0)))
 
 (provide 'init-ui)
