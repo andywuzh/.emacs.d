b4c04b2f689784f2c7395379feefd526a33098f3
Wu Yaohua <wuyaohua@spicespirit.com>
2020-12-24 17:34:31 +0800

    use package switch-window

--- lisp/init-kbd.el
+++ lisp/init-kbd.el
@@ -21,4 +21,22 @@
          ("C-c d" . crux-duplicate-current-line-or-region)
          ("C-c k" . crux-smart-kill-line)))
 
+(use-package switch-window
+  :init
+  (setq switch-window-threshold 3)
+  :bind (("C-x o" . switch-window)
+         ("C-x 1" . switch-window-then-maximize)
+         ("C-x 2" . switch-window-then-split-below)
+         ("C-x 3" . switch-window-then-split-right)
+         ("C-x 0" . switch-window-then-delete)
+
+         ("C-x 4 d" . switch-window-then-dired)
+         ("C-x 4 f" . switch-window-then-find-file)
+         ("C-x 4 r" . switch-window-then-find-file-read-only)
+
+         ("C-x 4 C-f" . switch-window-then-find-file)
+         ("C-x 4 C-o" . switch-window-then-display-buffer)
+
+         ("C-x 4 0" . switch-window-then-kill-buffer)))
+
 (provide 'init-kbd)
