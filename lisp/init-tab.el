5aecb92e5d1c0a29add90c5a6087f6932e0cb5f3
andywu <cola1129@gmail.com>
2025-12-11 10:10:30 +0800

    remove awesome-tab

--- lisp/init-tab.el
+++ lisp/init-tab.el
@@ -5,35 +5,35 @@
 ;;; Code:
 
 ;; tab
-(use-package awesome-tab
-  :load-path "site-lisp/awesome-tab"
-  :demand
-  :bind
-  (:map awesome-tab-mode-map
-        ("C-<tab>" . awesome-tab-forward)
-        ("C-<iso-lefttab>" . awesome-tab-backward))
-  :config
-  (defun awesome-tab-hide-tab (x)
-    (let ((name (format "%s" x)))
-      (or
-       ;; Hide tab if current window is not dedicated window.
-       (window-dedicated-p (selected-window))
+;; (use-package awesome-tab
+;;   :load-path "site-lisp/awesome-tab"
+;;   :demand
+;;   :bind
+;;   (:map awesome-tab-mode-map
+;;         ("C-<tab>" . awesome-tab-forward)
+;;         ("C-<iso-lefttab>" . awesome-tab-backward))
+;;   :config
+;;   (defun awesome-tab-hide-tab (x)
+;;     (let ((name (format "%s" x)))
+;;       (or
+;;        ;; Hide tab if current window is not dedicated window.
+;;        (window-dedicated-p (selected-window))
 
-       (string-prefix-p "*Completions*" name)
-       ;; (string-prefix-p "*Minibuf" name)
-       (string-prefix-p "*lsp-bridge" name)
+;;        (string-prefix-p "*Completions*" name)
+;;        ;; (string-prefix-p "*Minibuf" name)
+;;        (string-prefix-p "*lsp-bridge" name)
 
-       (string-prefix-p "*sdcv" name)
-       (string-prefix-p "*helm" name)
-       (string-prefix-p "*flycheck" name)
+;;        (string-prefix-p "*sdcv" name)
+;;        (string-prefix-p "*helm" name)
+;;        (string-prefix-p "*flycheck" name)
 
-       ;; Hide blacklist if emacs version < 27 (use header-line).
-       (and (eq awesome-tab-display-line 'header-line)
-            (or (string-prefix-p "*Compile-Log*" name)
-                (string-prefix-p "*Flycheck" name)))
-       )))
-  ;; (awesome-tab-mode t)
-  )
+;;        ;; Hide blacklist if emacs version < 27 (use header-line).
+;;        (and (eq awesome-tab-display-line 'header-line)
+;;             (or (string-prefix-p "*Compile-Log*" name)
+;;                 (string-prefix-p "*Flycheck" name)))
+;;        )))
+;;   ;; (awesome-tab-mode t)
+;;   )
 
 
 
