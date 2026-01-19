8968443b622f50adc2fcc8b99f5d012eae0ff87f
andywu <cola1129@gmail.com>
2026-01-19 17:33:49 +0800

    add ai-code

--- lisp/init-ai.el
+++ lisp/init-ai.el
@@ -16,5 +16,15 @@
         ("C-c c" . claude-code-transient))
   )
 
+;; AI Code Interface
+(use-package ai-code
+  :config
+  (ai-code-set-backend 'opencode)
+  (global-set-key (kbd "C-c a") #'ai-code-menu)
+  (global-auto-revert-mode 1)
+  (setq auto-revert-interval 1)
+  (with-eval-after-load 'magit
+    (ai-code-magit-setup-transients)))
+
 (provide 'init-ai)
 ;;; init-ai.el ends here
